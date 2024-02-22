8c82b1a306468f278efa9089d32456ac8be52c84
Robert Wyszynski <robertointerwento@gmail.com>
2024-02-22 13:38:19 +0100

    add models

--- shop/models.py
+++ shop/models.py
@@ -4,13 +4,13 @@ from django.forms import CharField
 
 
 class Products(models.Model):
-    brand = models.CharField(blank=False)
+    brand = models.CharField(max_length=20, blank=False)
     model = models.CharField(max_length=32, blank=False, unique=True)
     voltage = models.PositiveSmallIntegerField(default=24)
-    power = models.PositiveSmallIntegerField()
+    power = models.PositiveSmallIntegerField(blank=True)
     current = models.DecimalField(max_digits=6, decimal_places=2)
-    protocol = models.CharField(blank=True)
+    protocol = models.CharField(max_length=10, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return self.model
+        return self.model()
--- smarthomeshop/settings.py
+++ smarthomeshop/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'smarthomeshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'components',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
