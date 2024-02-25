c40ddad71fbe0ee288315b77878ba60eb147d67e
Robert Wyszynski <robertointerwento@gmail.com>
2024-02-25 22:31:28 +0100

    add admin section

--- shop/models.py
+++ shop/models.py
@@ -8,9 +8,9 @@ class Products(models.Model):
     model = models.CharField(max_length=32, blank=False, unique=True)
     voltage = models.PositiveSmallIntegerField(default=24)
     power = models.PositiveSmallIntegerField(blank=True)
-    current = models.DecimalField(max_digits=6, decimal_places=2)
+    current = models.DecimalField(max_digits=6, decimal_places=1, blank=True)
     protocol = models.CharField(max_length=10, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return self.model()
+        return str(self.model)
